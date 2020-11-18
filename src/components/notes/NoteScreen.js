c30779959976f9f3a2b24f8d23006ccd6caafbc3
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-11-18 00:05:15 -0700

    Conclución diseño de NotesScrean

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
   return (
     <div  className="notes__main-content">
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input 
+          type="text"
+          placeholder="Title"
+          className="notes__title-input"
+          autoComplete="off"          
+        />
+
+        <textarea
+          placeholder="Write something you want :)"
+          className="notes__textarea"
+        >         
+        </textarea>
+        <div className="notes__image">
+          <img 
+            src="https://i.pinimg.com/originals/7b/f9/d5/7bf9d59941b52934e6b9e5a9a3ddd839.jpg"
+            alt="image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
