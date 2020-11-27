1cca0022e779b5c5cb7fc698ca4a6b4e5bf3f0f2
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-11-26 17:38:06 -0700

    [AUTH] Mantenemos los datos de la sesión en Redux aun que hayamos recargado el navegador, etc.

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { 
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 // Componentes!
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from './../components/journal/JournalScreen';
+import { firebase } from '../firebase/firebaseConfig';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+  
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    firebase.auth()
+      .onAuthStateChanged( user => {
+        if( user?.uid ) {
+          dispatch( login(user.uid, user.displayName) );
+        }
+      });
+  }, []);
+
   return (
     <Router>
       <div>
